2ddc6a25ed411789704314b457cbfc7069a88c18
Akoszewski <akoszews@gmail.com>
2023-09-24 00:40:39 +0200

    Image display

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_image.h>
+#include <stdio.h>
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+int main()
+{
+    al_init();
+    al_install_keyboard();
+
+    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
+    ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
+
+    ALLEGRO_DISPLAY* disp = al_create_display(SCREEN_WIDTH, SCREEN_WIDTH);
+
+    ALLEGRO_FONT* font = al_create_builtin_font();
+
+    al_register_event_source(queue, al_get_keyboard_event_source());
+    al_register_event_source(queue, al_get_display_event_source(disp));
+    al_register_event_source(queue, al_get_timer_event_source(timer));
+
+    bool redraw = true;
+    ALLEGRO_EVENT event;
+
+    al_start_timer(timer);
+
+    if (!al_init_image_addon()) {
+        printf("couldn't initialize image addon\n");
+        return 1;
+    }
+
+    ALLEGRO_BITMAP* image = al_load_bitmap("pictures/roi.bmp");
+    if (!image) {
+        printf("Couldn't load image\n");
+        return 1;
+    }
+
+    while (1)
+    {
+        al_wait_for_event(queue, &event);
+        if (event.type == ALLEGRO_EVENT_TIMER) {
+            redraw = true;
+        } else if((event.type == ALLEGRO_EVENT_KEY_DOWN) || (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)) {
+            break;
+        }
+
+        if (redraw && al_is_event_queue_empty(queue)) {
+            al_clear_to_color(al_map_rgb(0, 50, 255));
+            // al_draw_text(font, al_map_rgb(255, 255, 255), 0, 0, 0, "Hello world!");
+            al_draw_bitmap(image, (SCREEN_WIDTH - al_get_bitmap_width(image))/2, (SCREEN_HEIGHT - al_get_bitmap_height(image))/2, 0);
+            al_flip_display();
+
+            redraw = false;
+        }
+    }
+
+    al_destroy_bitmap(image);
+    al_destroy_font(font);
+    al_destroy_display(disp);
+    al_destroy_timer(timer);
+    al_destroy_event_queue(queue);
+
+    return 0;
+}
